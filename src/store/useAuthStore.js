969bd0ef3f150014bb9331206a77390d8fcd58dc
Virendra Yadav <virendrayadav1292000@gmail.com>
2025-06-20 18:34:13 +0530

    fix the env file

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -7,7 +7,7 @@ import { io } from "socket.io-client";
 //For local developemnt purpose.
 // const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const BASE_URL = import.meta.env.BACKEND_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
