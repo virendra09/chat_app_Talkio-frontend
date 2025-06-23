ddc6d645764522abc634c6b9f6636d996141b30d
Virendra Yadav <virendrayadav1292000@gmail.com>
2025-06-23 18:54:20 +0530

    fix the useAuthStore file

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -2,14 +2,12 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
-import { useNavigate } from "react-router-dom";
 
 
 //For local developemnt purpose.
 // const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-const navigate = useNavigate();
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
@@ -69,7 +67,6 @@ export const useAuthStore = create((set, get) => ({
       get().disconnectSocket();
       toast.success("Logged out successfully");
       
-      navigate("/login");
     } catch (error) {
       toast.error(error.response.data.message);
     }
